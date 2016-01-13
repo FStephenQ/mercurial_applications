36be6ed029218cabcf56e25aa85d2a1ba8e59c8e
F. Stephen Quaratiello <root@mail.mercuryq.net>
2016-01-13 10:50:18 -0500

    new file:   core_page/__init__.pyc 	modified:   core_page/views.py 	new file:   core_page/views.pyc 	renamed:    core_page/templates/index.html -> templates/core_page/index.html

--- core_page/views.py
+++ core_page/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response("index.html")
+    return render_to_response("core_page/index.html")
 
 def bio(request):
 	return HttpResponse("There's nothing here yet!")
