ba2d7bbd214ce5a08f06d0a731763d7d16b8fd8d
F. Stephen Quaratiello <fstephen@mercurialapplications.com>
2016-01-14 11:30:26 -0500

    modified:   blog/views.py 	modified:   templates/blog/front.html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 # Create your views here.
 from django.http import HttpResponse
-
+from django.template import Template
+from django.shortcuts import render_to_response
+from mercurial_applications.blog import models
 
 def front(request):
-	return HttpResponse("Theres nothing here yet!")
+        posts = models.Post.objects.all()
+        return render_to_response('blog/front.html',{'posts':posts,})
